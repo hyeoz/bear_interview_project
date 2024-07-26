8e46ae11b24fc5b858528e0d377af53d3ddafc9e
hyeoz <jenny970203@gmail.com>
2024-07-26 13:54:48 +0900

    init: project initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SetupWorkerApi } from "msw";
+
+if (process.env.NODE_ENV === "development") {
+  const { worker }: { worker: SetupWorkerApi } = require("./mocks/browser");
+  worker.start({ onUnhandledRequest: "bypass" });
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
